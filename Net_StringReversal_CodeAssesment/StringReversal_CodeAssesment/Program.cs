dcc78232788ce453a2b7c4a2c8c8ca83b0b37014
Thapelo Sekwena <sekwena.thapelo@yahoo.com>
2021-01-15 15:48:58 +0200

    First Commit

---
+++ Net_StringReversal_CodeAssesment/StringReversal_CodeAssesment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace StringReversal_CodeAssesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string you would like to reverse? ");
+            String inputStr = Console.ReadLine();
+           
+            Console.WriteLine("\nInput string: " + inputStr);
+           
+
+            Console.WriteLine("Output string: " + ReverseString(inputStr).ToString());
+        }
+
+
+        public static string ReverseString(string inputStr)
+        {
+
+            char[] charArray = inputStr.ToCharArray();
+
+            int end = charArray.Length - 1, start = 0;
+
+            while (start < end)
+            {
+                if (!char.IsLetter(charArray[start]))
+                    start++;
+                else if (!char.IsLetter(charArray[end]))
+                    end--;
+
+                else
+                {
+                    char tempChar = charArray[start];
+                    charArray[start] = charArray[end];
+                    charArray[end] = tempChar;
+                    start++;
+                    end--;
+                }
+            }
+            return new string(charArray);
+        }
+    }
+}
